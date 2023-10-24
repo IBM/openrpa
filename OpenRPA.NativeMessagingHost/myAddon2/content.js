edc540dffac88207f2651e6298e2984010097e3a
Renato Souza <Renato.Souza@ibm.com>
2023-10-24 15:40:19 -0300

    fix(plugin): use composed path function to get the correct event target
    Signed-off-by: Renato Souza <Renato.Souza@ibm.com>

--- OpenRPA.NativeMessagingHost/myAddon2/content.js
+++ OpenRPA.NativeMessagingHost/myAddon2/content.js
@@ -709,7 +709,12 @@ if (true == false) {
                         // https://stackoverflow.com/questions/3437786/get-the-size-of-the-screen-current-web-page-and-browser-window
                         let message = { functionName: action, frame: frame, parents: 0, xpaths: [] };
                         let targetElement = null;
-                        targetElement = event.target || event.srcElement;
+                        try {
+                            targetElement = event.composedPath()[0];
+                        } catch (e) {
+                            console.error('event composed path');
+                        }
+                        targetElement = targetElement || event.target || event.srcElement;
                         if (targetElement == null) {
                             console.log('targetElement == null');
                             return;
